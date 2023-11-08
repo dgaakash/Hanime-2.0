163ddcb015b7ace81a284d82977f50539dca9c1e
Otakubhai <87870903+Otakubhai@users.noreply.github.com>
2023-11-08 20:45:23 +0530

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -10,7 +10,7 @@ async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
     if data == "about":
         await query.message.edit_text(
-            text = f"<b>○ Creator : <a href='tg://user?id={OWNER_ID}'>This Person</a>\n○ My Owner : @Call_Me_Blank\n○ Channel : @WebCinematics\n○ Support Group : @WebCinematics_Support_Bot</b>",
+            text = f"<b>○ Sabka Malik : <a href='tg://user?id={OWNER_ID}'>@Call_Me_Blank</a>\n○ Visit this too : @WebCinematics\n○ Channel : @Otaku_Library \n○ Support Group : @Otaku_Chats \n○ Harem Animes at : @Harem_Library  \n○Hanimes at : @Hanime_Library</b>",
             disable_web_page_preview = True,
             reply_markup = InlineKeyboardMarkup(
                 [
